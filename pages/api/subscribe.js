29d7a706cb762ed019ee848e30ae8faccaac765c
Eetu Eskelinen <edwardtehgreat@gmail.com>
2022-01-17 23:05:15 +0200

    Finish susbcription API route

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,11 +1,10 @@
-// Adding support for buttondown subscriptions: https://leerob.io/snippets/buttondown
-
-// export default function handler(req, res) {
-//   res.status(200).json({ name: "John Doe" });
-// }
+// Adding support for buttondown subscriptions. Based on https://leerob.io/snippets/buttondown
 
 export default async (req, res) => {
   const { email } = req.body;
+  const tags = ["Nox"];
+  console.log(`Subscribing ${email} to ${tags}`);
+  console.log(JSON.stringify({ email, tags }));
 
   if (!email) {
     return res.status(400).json({ error: "Email is required" });
@@ -16,7 +15,7 @@ export default async (req, res) => {
     const response = await fetch(
       `https://api.buttondown.email/v1/subscribers`,
       {
-        body: JSON.stringify({ email }),
+        body: JSON.stringify({ email, tags }),
         headers: {
           Authorization: `Token ${API_KEY}`,
           "Content-Type": "application/json",
@@ -31,7 +30,7 @@ export default async (req, res) => {
       });
     }
 
-    return res.status(201).json({ error: "" });
+    return res.status(201).json({ msg: `success` });
   } catch (error) {
     return res.status(500).json({ error: error.message || error.toString() });
   }
