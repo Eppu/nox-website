1256851f43e86396eb58a2bf41c40ade0bf73675
Eetu Eskelinen <eetuesk@gmail.com>
2022-01-18 16:33:22 +0200

    Add subscription component

--- components/nav.js
+++ components/nav.js
@@ -13,21 +13,6 @@ const Nav = () => {
           className="inline-flex items-center"
         >
           <a>
-            {/* <svg
-            className="w-8 text-deep-purple-accent-400"
-            viewBox="0 0 24 24"
-            stroke-linejoin="round"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-miterlimit="10"
-            stroke="currentColor"
-            fill="none"
-          >
-            <rect x="3" y="1" width="7" height="12"></rect>
-            <rect x="3" y="17" width="7" height="6"></rect>
-            <rect x="14" y="1" width="7" height="6"></rect>
-            <rect x="14" y="11" width="7" height="12"></rect>
-          </svg> */}
             <span className="ml-2 text-xl font-bold tracking-wide text-teal-accent-700 uppercase">
               Nox
             </span>
