e299935272a82e4961b4349facc7fa5cd04bf46e
Eetu Eskelinen <edwardtehgreat@gmail.com>
2022-01-18 16:47:19 +0200

    Update nav

--- components/Nav.js
+++ components/Nav.js
@@ -2,7 +2,6 @@ import Link from "next/link";
 import { useState } from "react";
 
 const Nav = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
       <div className="relative flex items-center justify-between">
